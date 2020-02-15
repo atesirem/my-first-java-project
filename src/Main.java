23afb5ed57589734651d53f600be139f5840627f
Irem Ates <atesv26@gmail.com>
2020-02-15 17:09:41 +0300

    main kısmı dolduruldu

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
 
 public class Main {
 
+	public static void main(String[] args) {
+
+		System.out.println();
+		System.out.println();
+		int x = 5;
+		double a = 1.3;
+
+		String y = "iroş love";
+
+	}
+
 }
